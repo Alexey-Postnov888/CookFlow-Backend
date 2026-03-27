fb255fa1e48aa160d42528d8da1172741a97796d
Alexey Postnov <adpostnov@yandex.ru>
2026-03-28 00:30:03 +0500

    added authorization with Keycloak; added example of work with authorization in categories.php

---
+++ src/config/main.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'keycloak_url' => env('KEYCLOAK_URL'),
+    'realm' => env('REALM'),
+    'client_id' => env('CLIENT_ID'),
+    'client_secret' => env('CLIENT_SECRET'),
+];
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth.keycloak'])->group(function () {
+//    Route::prefix('users')->group(base_path('routes/users.php'));
+    Route::prefix('categories')->group(base_path('routes/categories.php'));
+});
