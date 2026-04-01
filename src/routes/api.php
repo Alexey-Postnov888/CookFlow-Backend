443162b53bdeac21228bf9a7246c309a1ca3fbad
Andrey <eldotefc@gmail.com>
2026-04-02 03:22:17 +0500

    Изменил изменил методы в контроллере категорий, добавил обработку ошибок

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,14 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth.keycloak'])->group(function () {
-    Route::prefix('categories')->group(base_path('routes/auth/categories.php'));
     Route::prefix('recipes')->group(base_path('routes/auth/comments.php'));
     Route::prefix('recipes')->group(base_path('routes/auth/recipes.php'));
     Route::prefix('users')->group(base_path('routes/auth/users.php'));
     Route::prefix('recipes/categories')->group(base_path('routes/auth/categories.php'));
     Route::prefix('favourites')->group(base_path('routes/auth/favourites.php'));
 });
-Route::prefix('recipes')->group(base_path('routes/categories.php'));
+Route::prefix('recipes/categories')->group(base_path('routes/categories.php'));
 Route::prefix('recipes')->group(base_path('routes/comments.php'));
 Route::prefix('recipes')->group(base_path('routes/recipes.php'));
 Route::prefix('users')->group(base_path('routes/users.php'));
--- src/routes/categories.php
+++ src/routes/categories.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/categories', [CategoryController::class, 'getCategories']);
+Route::get('/', [CategoryController::class, 'getCategories']);
